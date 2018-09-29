64906583c1b22827f93f37c7b73927f7d4eeb615
isihaxjan <40036128+isihaxjan@users.noreply.github.com>
2018-09-29 02:46:01 +0200

    first

---
+++ js/fish.js
@@ -0,0 +1,16 @@
+class Fish {
+  constructor() {
+    this.x = W/2;
+    this.y = H/2;
+  }
+
+  draw(c) {
+
+  }
+
+  update() {
+
+  }
+
+
+}
---
+++ js/main.js
@@ -0,0 +1,59 @@
+var W = 800;
+var H = 600;
+var INTERVAL = 15;
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var res = 50;
+var grid = [], rows = Math.floor(W / res), cols = Math.floor(H / res);
+var fish;
+/*
+Ok so:
+- Grid (just cuz i like grids);
+- Fish movement randomly
+- Fish food with mouse on click
+- Fish food bar
+*/
+
+window.onload = function() {
+
+  for(var i; i < rows; i++) {
+    for(var j; j < cols; j++) {
+      var cell = new Cell(i, j);
+      grid.push(cell);
+    }
+  }
+
+  setInterval(() => {
+
+  }, INTERVAL);
+
+}
+
+function Cell(i, j) {
+  this.i = i;
+  this.j = j;
+
+  this.show = function(c) {
+    this.x = this.i*res;
+    this.y = this.j*res;
+
+    c.beginPath();
+    c.rect(this.x, this.y, res, res);
+    c.stroke();
+    c.fillStyle === "#1a7a3f";
+    c.fill();
+
+  }
+}
+
+function draw() {
+
+}
+
+function drawCanvas() {
+
+}
+
+function update() {
+
+}
